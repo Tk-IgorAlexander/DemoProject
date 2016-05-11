851e122a337ced42428c3978541cf1ad139d50f5
Igor Roldan <igor.roldan@tektonlabs.com>
2016-05-11 17:43:36 +0000

    S1: bookReserve: Migration and models

---
+++ database/migrations/2016_05_11_172553_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('title');
+            $table->text('desc');
+            $table->char('isbn', 13);
+            $table->string('image_path');
+            $table->tinyInteger('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
